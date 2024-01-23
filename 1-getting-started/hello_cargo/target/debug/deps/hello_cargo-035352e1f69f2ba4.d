edce18c4104a5901cfb38820d4c49ce20a8f35de
yantavares <yantdo1@gmail.com>
2024-01-23 10:32:22 -0300

    Delete unused files and add .gitignore

--- 1-getting-started/hello_cargo/target/debug/deps/hello_cargo-035352e1f69f2ba4.d
+++
@@ -1,5 +0,0 @@
-/Users/yantavares/rust-book/chapter1/hello_cargo/target/debug/deps/hello_cargo-035352e1f69f2ba4: src/main.rs
-
-/Users/yantavares/rust-book/chapter1/hello_cargo/target/debug/deps/hello_cargo-035352e1f69f2ba4.d: src/main.rs
-
-src/main.rs:
--- 1-getting-started/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/yantavares/rust-book/chapter1/hello_cargo/target/debug/hello_cargo: /Users/yantavares/rust-book/chapter1/hello_cargo/src/main.rs
