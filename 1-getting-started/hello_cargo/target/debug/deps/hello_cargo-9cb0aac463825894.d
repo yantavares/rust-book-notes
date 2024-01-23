f06a4128fefad90d74cd9e01836ab45c93635a4b
yantavares <yantdo1@gmail.com>
2024-01-23 10:31:03 -0300

    Add initial code for hello_cargo project

---
+++ 1-getting-started/hello_cargo/target/debug/deps/hello_cargo-9cb0aac463825894.d
@@ -0,0 +1,5 @@
+/Users/yantavares/rust-book/chapter1/hello_cargo/target/debug/deps/libhello_cargo-9cb0aac463825894.rmeta: src/main.rs
+
+/Users/yantavares/rust-book/chapter1/hello_cargo/target/debug/deps/hello_cargo-9cb0aac463825894.d: src/main.rs
+
+src/main.rs:
---
+++ 1-getting-started/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/yantavares/rust-book/chapter1/hello_cargo/target/debug/hello_cargo: /Users/yantavares/rust-book/chapter1/hello_cargo/src/main.rs
