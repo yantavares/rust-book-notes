b673ce55443b21452efc56ac71dbb7cf6e7fb70a
yantavares <yantdo1@gmail.com>
2024-01-23 10:26:29 -0300

    Add .gitignore file to ignore target directory

--- 2-guessing-game/guessing_game/target/debug/deps/guessing_game-a25eb97511d7c681.d
+++
@@ -1,5 +0,0 @@
-/Users/yantavares/rust-book/2-guessing-game/guessing_game/target/debug/deps/guessing_game-a25eb97511d7c681: src/main.rs
-
-/Users/yantavares/rust-book/2-guessing-game/guessing_game/target/debug/deps/guessing_game-a25eb97511d7c681.d: src/main.rs
-
-src/main.rs:
--- 2-guessing-game/guessing_game/target/debug/deps/guessing_game-ac02eb633911409c.d
+++
@@ -1,5 +0,0 @@
-/Users/yantavares/rust-book/2-guessing-game/guessing_game/target/debug/deps/libguessing_game-ac02eb633911409c.rmeta: src/main.rs
-
-/Users/yantavares/rust-book/2-guessing-game/guessing_game/target/debug/deps/guessing_game-ac02eb633911409c.d: src/main.rs
-
-src/main.rs:
