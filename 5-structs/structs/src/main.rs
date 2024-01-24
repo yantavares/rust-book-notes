78e73cf91f002892066a10a974ddeeec50c91ead
yantavares <yantdo1@gmail.com>
2024-01-24 09:16:35 -0300

    Add User struct and update username in main function

--- 5-structs/structs/src/main.rs
+++ 5-structs/structs/src/main.rs
@@ -1,3 +1,19 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    is_active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = User {
+        username: String::from("yan"),
+        email: String::from("yan@email.com"),
+        sign_in_count: 1,
+        is_active: true,
+    };
+
+    let name = user1.username;
+
+    user1.username = String::from("Yan");
 }
