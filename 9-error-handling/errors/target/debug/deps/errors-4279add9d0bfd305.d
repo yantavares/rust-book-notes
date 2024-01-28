137769452c9d9cedd2501860d89dad4e05635e15
yantavares <yantdo1@gmail.com>
2024-01-28 19:54:50 -0300

    Added chapter 9

---
+++ 9-error-handling/errors/target/debug/deps/errors-4279add9d0bfd305.d
@@ -0,0 +1,5 @@
+/Users/yantavares/rust-book/9-error-handling/errors/target/debug/deps/liberrors-4279add9d0bfd305.rmeta: src/main.rs
+
+/Users/yantavares/rust-book/9-error-handling/errors/target/debug/deps/errors-4279add9d0bfd305.d: src/main.rs
+
+src/main.rs:
---
+++ 9-error-handling/errors/target/debug/deps/errors-7aa4e1cbadfb075a.d
@@ -0,0 +1,5 @@
+/Users/yantavares/rust-book/9-error-handling/errors/target/debug/deps/liberrors-7aa4e1cbadfb075a.rmeta: src/main.rs
+
+/Users/yantavares/rust-book/9-error-handling/errors/target/debug/deps/errors-7aa4e1cbadfb075a.d: src/main.rs
+
+src/main.rs:
