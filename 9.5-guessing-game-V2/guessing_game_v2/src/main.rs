cbf495d1ac2697e1cc7726ff3d6b535275118fe5
yantavares <yantdo1@gmail.com>
2024-01-28 21:45:33 -0300

    new guessing game 2

--- 9.5-guessing-game-V2/guessing_game_v2/src/main.rs
+++ 9.5-guessing-game-V2/guessing_game_v2/src/main.rs
@@ -3,27 +3,58 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Result<Guess, String> {
+        if value < 1 || value > 100 {
+            return Err(format!(
+                "Guess value must be between 1 and 100, got {}.",
+                value
+            ));
+        }
+
+        Ok(Guess { value })
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
-    let secret_number: u32 = rand::thread_rng().gen_range(1..101);
+    let secret_number: i32 = rand::thread_rng().gen_range(1..101);
 
     loop {
         println!("Please input your guess.");
 
-        let mut guess: String = String::new();
+        let mut guess_str = String::new();
 
         io::stdin()
-            .read_line(&mut guess)
+            .read_line(&mut guess_str)
             .expect("Failed to read line");
 
-        // Variable Shadowing
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess_str.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
         };
 
-        println!("You guessed: {}", guess);
+        let guess = match Guess::new(guess) {
+            Ok(g) => g,
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        };
 
-        match guess.cmp(&secret_number) {
+        println!("You guessed: {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("{}", "Too small...".blue()),
             Ordering::Greater => println!("{}", "Too big!".red()),
             Ordering::Equal => {
