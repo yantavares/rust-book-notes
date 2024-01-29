e781f9a5b0f2cb4a4bd8ac9f9327c0158a0f4ca8
yantavares <yantdo1@gmail.com>
2024-01-28 21:36:49 -0300

    added new guessing game

---
+++ 9.5-guessing-game-V2/guessing_game_v2/README.md
@@ -0,0 +1,3 @@
+# Guessing Game V2
+
+This is basically the same as the [Guessing Game](../2-guessing-game/README.md), but with error handling.
--- 9.5-guessing-game-V2/guessing_game_v2/src/main.rs
+++ 9.5-guessing-game-V2/guessing_game_v2/src/main.rs
@@ -1,3 +1,35 @@
+use colored::Colorize;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number: u32 = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // Variable Shadowing
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small...".blue()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!!!".green());
+                break;
+            }
+        }
+    }
 }
