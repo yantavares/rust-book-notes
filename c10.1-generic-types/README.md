a0f1174cd156fe2481bb4a89aec914718171709e
yantavares <yantdo1@gmail.com>
2024-01-30 11:43:40 -0300

    ch 10.1 done

--- c10.1-generic-types/README.md
+++ c10.1-generic-types/README.md
@@ -1,1 +1,61 @@
-# Chapter 10 - Generic Types in Rust
+# Chapter 10.1 - Generic Types in Rust
+
+## What are Generic Types in Rust?
+- Generic types are a way to define functions or structs that can work with any type.
+- Generics are defined using angle brackets `<>`, and the type is specified inside these brackets.
+
+## Using Generic Types in Functions
+- You can define generic functions in Rust to work with different types.
+- Here's an example of a generic function that finds the largest element in a list of any type:
+  ```rust
+  fn largest<T>(list: &[T]) -> T {
+      let mut largest = list[0];
+
+      for &item in list.iter() {
+          if item > largest {
+              largest = item;
+          }
+      }
+
+      largest
+  }
+  ```
+
+- You can also have multiple generic types in a function like this:
+  ```rust
+  fn largest<T, U>(list1: &[T], list2: &[U]) -> T {
+      let mut largest = list1[0];
+
+      for &item in list1.iter() {
+          if item > largest {
+              largest = item;
+          }
+      }
+
+      largest
+  }
+  ```
+
+- Type restrictions can be applied to generic types using traits. For example, the `PartialOrd` and `Copy` traits can be used to ensure that the generic type can be compared and copied, respectively:
+  ```rust
+  fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+      let mut largest = list[0];
+
+      for &item in list.iter() {
+          if item > largest {
+              largest = item;
+          }
+      }
+
+      largest
+  }
+  ```
+
+## Using Generic Types in Structs
+- Generic types can also be used in structs. Here's an example of a Point struct with a generic type:
+  ```rust
+  struct Point<T> {
+      x: T,
+      y: T,
+  }
+  ```
--- c10.1-generic-types/generics/src/main.rs
+++ c10.1-generic-types/generics/src/main.rs
@@ -1,3 +1,103 @@
+// We can use generics in structs, enums, methods, and functions
+#[derive(Debug)]
+struct Point<T> {
+    _x: T,
+    _y: T,
+}
+
+// Will only work with f32 numbers
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self._x.powi(2) + self._y.powi(2)).sqrt()
+    }
+}
+
+// Will work with any type
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self._x
+    }
+}
+
+// We can have multiple generic types
+#[derive(Debug)]
+struct Point2<T, U> {
+    _x: T,
+    _y: U,
+}
+
+// Returns a Point2 with the x of self and the y of other
+// Has to change the types accordingly
+impl<T, U> Point2<T, U> {
+    fn mixup<V, W>(self, other: Point2<V, W>) -> Point2<T, W> {
+        Point2 {
+            _x: self._x,
+            _y: other._y,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 25, 100, 65];
+    // Only works with i32 numbers
+    let largest = get_largest_number(number_list);
+    println!("The largest number is {}", largest);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    // Generic typing
+    let largest = get_largest(char_list);
+    println!("The largest char is {}", largest);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    // Generic typing
+    let largest = get_largest(number_list);
+    println!("The largest number is {}", largest);
+
+    let integer = Point { _x: 5, _y: 10 };
+    let float = Point { _x: 1.0, _y: 4.0 };
+    println!("{:?}", integer);
+    println!("{:?}", float);
+
+    // ! let dist = integer.distance_from_origin(); Does not work
+    let dist = float.distance_from_origin();
+    println!("Distance from origin: {}", dist);
+
+    let x = integer.x();
+    println!("x is {}", x);
+
+    // Note that x and y are different types
+    let mixed = Point2 { _x: 5, _y: 4.0 };
+    println!("{:?}", mixed);
+
+    let mixed = mixed.mixup(Point2 {
+        _x: "hello",
+        _y: 'c',
+    });
+    println!("{:?}", mixed);
+}
+
+// Not generic
+fn get_largest_number(list: Vec<i32>) -> i32 {
+    let mut largest = list[0];
+
+    for &number in list.iter() {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
+}
+
+// Generic implementation
+fn get_largest<T: PartialOrd + Copy>(list: Vec<T>) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
