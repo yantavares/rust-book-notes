e27c6f03cc8de03ee29ce3aa8547e7a2ad74a431
yantavares <yantdo1@gmail.com>
2024-02-08 15:07:10 -0300

    removed target

--- chapters/15-smart-pointers/smart_pointers/target/debug/deps/smart_pointers-8078147dabc1ffac.d
+++
@@ -1,5 +0,0 @@
-/Users/yantavares/rust-book/15-smart-pointers/smart_pointers/target/debug/deps/libsmart_pointers-8078147dabc1ffac.rmeta: src/main.rs
-
-/Users/yantavares/rust-book/15-smart-pointers/smart_pointers/target/debug/deps/smart_pointers-8078147dabc1ffac.d: src/main.rs
-
-src/main.rs:
--- chapters/15-smart-pointers/smart_pointers/target/debug/smart_pointers.d
+++
@@ -1,1 +0,0 @@
-/Users/yantavares/rust-book/15-smart-pointers/smart_pointers/target/debug/smart_pointers: /Users/yantavares/rust-book/15-smart-pointers/smart_pointers/src/main.rs
